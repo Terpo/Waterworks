75785817ff284bda698c584a3c7c9623b94f4c50
Terpo <mail@terpo.org>
2017-04-29 15:09:47 +0200

    added proper water texture to GUI
    - fix #3

--- src/main/java/org/terpo/waterworks/gui/GuiFluidContainer.java
+++ src/main/java/org/terpo/waterworks/gui/GuiFluidContainer.java
@@ -5,8 +5,11 @@ import org.terpo.waterworks.tileentity.TileEntityRainTankWood;
 import org.terpo.waterworks.tileentity.TileWaterworks;
 
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidRegistry;
 
 //Client Side
 public class GuiFluidContainer extends GuiContainerBase {
@@ -19,6 +22,7 @@ public class GuiFluidContainer extends GuiContainerBase {
 		if (te instanceof TileEntityRainTankWood) {
 			this.fluidTank = ((TileEntityRainTankWood) te).getFluidTank();
 		}
+
 	}
 
 	@Override
@@ -27,16 +31,16 @@ public class GuiFluidContainer extends GuiContainerBase {
 		GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 		this.mc.getTextureManager()
 				.bindTexture(new ResourceLocation("waterworks:textures/gui/container/rain_tank_wood.png"));
-		this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
+		this.drawTexturedModalRect(getGuiLeft(), getGuiTop(), 0, 0, getXSize(), getYSize());
 
 		if (this.fluidTank != null) {
 			final int fillHeight = this.fluidTank.getFluidAmount() * 52 / this.fluidTank.getCapacity();
 
-			this.mc.getTextureManager()
-					.bindTexture(new ResourceLocation("minecraft:textures/blocks/water_overlay.png"));
-			this.drawTexturedModalRect(this.guiLeft + 80, this.guiTop + 69 - fillHeight, 0, 0, 16, fillHeight);
+			final ResourceLocation waterResource = FluidRegistry.WATER.getStill();
+			final TextureAtlasSprite sprite = this.mc.getTextureMapBlocks().getAtlasSprite(waterResource.toString());
+			this.mc.renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+			this.drawTexturedModalRect(getGuiLeft() + 80, getGuiTop() + 69 - fillHeight, sprite, 16, fillHeight);
 		}
-
 	}
 
 	@Override
@@ -49,11 +53,11 @@ public class GuiFluidContainer extends GuiContainerBase {
 		final String tooltip = this.fluidTank.getFluidAmount() + "/" + this.fluidTank.getCapacity() + " mB";
 		final int stringWidth = this.fontRendererObj.getStringWidth(tooltip);
 
-		if (this.guiLeft + 80 < mouseX && mouseX < this.guiLeft + 96 && mouseY > this.guiTop + 16
-				&& mouseY < this.guiTop + 69) {
-			drawRect(mouseX - this.guiLeft + 5, mouseY - this.guiTop - 5, mouseX - this.guiLeft + 15 + stringWidth,
-					mouseY - this.guiTop + 15, 0xFF000000);// this.fluidTank.getFluid().getFluid().getColor()
-			this.fontRendererObj.drawString(tooltip, mouseX - this.guiLeft + 10, mouseY - this.guiTop, 0xAAAAAA); // #404040
+		if (getGuiLeft() + 80 < mouseX && mouseX < getGuiLeft() + 96 && mouseY > getGuiTop() + 16
+				&& mouseY < getGuiTop() + 69) {
+			drawRect(mouseX - getGuiLeft() + 5, mouseY - getGuiTop() - 5, mouseX - getGuiLeft() + 15 + stringWidth,
+					mouseY - getGuiTop() + 15, 0xFF000000);// this.fluidTank.getFluid().getFluid().getColor()
+			this.fontRendererObj.drawString(tooltip, mouseX - getGuiLeft() + 10, mouseY - getGuiTop(), 0xAAAAAA); // #404040
 		}
 
 	}
