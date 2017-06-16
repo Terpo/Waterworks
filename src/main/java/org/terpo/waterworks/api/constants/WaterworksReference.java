bff67e52f3f85db688323f82a234ed582d515178
Terpo <mail@terpo.org>
2017-06-16 23:54:34 +0200

    prepare for 1.0.2 release

--- src/main/java/org/terpo/waterworks/api/constants/WaterworksReference.java
+++ src/main/java/org/terpo/waterworks/api/constants/WaterworksReference.java
@@ -4,7 +4,7 @@ public class WaterworksReference {
 	public static final String MODID = "waterworks";
 	public static final String DOMAIN = MODID + ":";
 	public static final String NAME = "Waterworks";
-	public static final String VERSION = "1.11.2-1.0.1";
+	public static final String VERSION = "1.11.2-1.0.2";
 
 	public static final String CLIENTPROXY = "org.terpo.waterworks.proxy.ClientProxy";
 	public static final String SERVERPROXY = "org.terpo.waterworks.proxy.ServerProxy";
