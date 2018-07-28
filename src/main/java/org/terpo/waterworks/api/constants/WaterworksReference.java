c10bc652ad91c450ced3330f2566ab08715cac3d
Terpo <mail@terpo.org>
2018-07-28 22:54:31 +0200

    prepare update of 1.0.12 for 1.12.2

--- src/main/java/org/terpo/waterworks/api/constants/WaterworksReference.java
+++ src/main/java/org/terpo/waterworks/api/constants/WaterworksReference.java
@@ -4,7 +4,7 @@ public class WaterworksReference {
 	public static final String MODID = "waterworks";
 	public static final String DOMAIN = MODID + ":";
 	public static final String NAME = "Waterworks";
-	public static final String VERSION = "1.12.2-1.0.11";
+	public static final String VERSION = "1.12.2-1.0.12";
 
 	public static final String CLIENTPROXY = "org.terpo.waterworks.proxy.ClientProxy";
 	public static final String SERVERPROXY = "org.terpo.waterworks.proxy.ServerProxy";
