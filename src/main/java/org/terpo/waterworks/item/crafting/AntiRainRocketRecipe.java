17da393ece4837d8c8ec5f2cb8a7f1f02d134884
Terpo <mail@terpo.org>
2020-01-03 16:06:47 +0100

    fix recipes allow other items that get deleted, closes #70

--- src/main/java/org/terpo/waterworks/item/crafting/AntiRainRocketRecipe.java
+++ src/main/java/org/terpo/waterworks/item/crafting/AntiRainRocketRecipe.java
@@ -1,5 +1,8 @@
 package org.terpo.waterworks.item.crafting;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.terpo.waterworks.init.WaterworksConfig;
 import org.terpo.waterworks.init.WaterworksItems;
 
@@ -17,72 +20,101 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class AntiRainRocketRecipe extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
 	private ItemStack resultItem = ItemStack.EMPTY;
+	private static final String NBT_ANTIRAIN = "ANTIRAIN";
+
+	private final List<Item> validItems;
+
+	public AntiRainRocketRecipe() {
+		super();
+		this.validItems = Arrays.asList(Items.FIREWORKS, WaterworksItems.firework_anti_rain,
+				Item.getItemFromBlock(Blocks.SPONGE));
+	}
+
 	@Override
 	public boolean matches(InventoryCrafting inv, World worldIn) {
 		this.resultItem = ItemStack.EMPTY;
 
 		int rocketStack = -1;
-		boolean isFireworks = false;
-		boolean isAntiRainRocket = false;
+		int isFireworks = 0;
+		int isAntiRainRocket = 0;
 		int multiplierAdd = 0;
 		for (int i = 0; i < inv.getSizeInventory(); i++) {
-			final ItemStack itemstack = inv.getStackInSlot(i);
+			final ItemStack stack = inv.getStackInSlot(i);
+			final Item item = stack.getItem();
 
-			if (itemstack.getItem() == Items.FIREWORKS) {
-				isFireworks = true;
-				rocketStack = i;
-				continue;
+			if (validateStack(stack, item)) {
+				return false;
 			}
-			if (itemstack.getItem() == WaterworksItems.firework_anti_rain) {
-				isAntiRainRocket = true;
-				rocketStack = i;
-				continue;
+
+			if (item == Items.FIREWORKS) {
+				isFireworks++;
 			}
-			if (itemstack.getItem() == Item.getItemFromBlock(Blocks.SPONGE) && itemstack.getMetadata() == 0) {
+
+			if (item == WaterworksItems.firework_anti_rain) {
+				isAntiRainRocket++;
+			}
+
+			if (item == Items.FIREWORKS || item == WaterworksItems.firework_anti_rain) {
+				rocketStack = i;
+			} else if (item == Item.getItemFromBlock(Blocks.SPONGE)) {
 				multiplierAdd++;
 			}
 		}
 
-		if ((isAntiRainRocket && isFireworks) || multiplierAdd <= 0) {
+		if ((isAntiRainRocket + isFireworks) > 1 || multiplierAdd == 0) {
 			return false;
 		}
+
 		this.resultItem = new ItemStack(WaterworksItems.firework_anti_rain);
 
-		int multiplierOld = 0;
-		if (isFireworks) {
-			final ItemStack rocket = inv.getStackInSlot(rocketStack);
-			NBTTagCompound nbtCompound = rocket.getTagCompound();
-
-			if (nbtCompound == null) {
-				nbtCompound = new NBTTagCompound();
-			}
-			if (multiplierOld + multiplierAdd > WaterworksConfig.rockets.clearSkyMaxMultiplier) {
-				return false;
-			}
-			nbtCompound.setInteger("ANTIRAIN", multiplierOld + multiplierAdd);
-			this.resultItem.setTagCompound(nbtCompound);
-			return true;
+		if (isFireworks > 0) {
+			return handleFireworksRocket(inv, rocketStack, multiplierAdd);
 		}
 
-		if (isAntiRainRocket) {
-			final ItemStack rocket = inv.getStackInSlot(rocketStack);
-			final NBTTagCompound nbtCompound = rocket.getTagCompound();
-			NBTTagCompound newTag = new NBTTagCompound();
-			if (nbtCompound != null) {
-				newTag = nbtCompound.copy();
-				if (nbtCompound.hasKey("ANTIRAIN")) {
-					multiplierOld = nbtCompound.getInteger("ANTIRAIN");
-				}
-			}
-			if ((multiplierOld + multiplierAdd) > WaterworksConfig.rockets.clearSkyMaxMultiplier) {
-				return false;
-			}
-			newTag.setInteger("ANTIRAIN", multiplierOld + multiplierAdd);
-			this.resultItem.setTagCompound(newTag);
-			return true;
+		if (isAntiRainRocket > 0) {
+			return handleAntiRainRocket(inv, rocketStack, multiplierAdd);
 		}
 
 		return false;
+	}
+
+	protected boolean validateStack(final ItemStack stack, final Item item) {
+		return !stack.isEmpty() && !this.validItems.contains(item);
+	}
+
+	protected boolean handleFireworksRocket(InventoryCrafting inv, int rocketStack, int multiplierAdd) {
+		final int multiplierOld = 0;
+		final ItemStack rocket = inv.getStackInSlot(rocketStack);
+		NBTTagCompound nbtCompound = rocket.getTagCompound();
+
+		if (nbtCompound == null) {
+			nbtCompound = new NBTTagCompound();
+		}
+		if (multiplierOld + multiplierAdd > WaterworksConfig.rockets.clearSkyMaxMultiplier) {
+			return false;
+		}
+		nbtCompound.setInteger(NBT_ANTIRAIN, multiplierOld + multiplierAdd);
+		this.resultItem.setTagCompound(nbtCompound);
+		return true;
+	}
+
+	protected boolean handleAntiRainRocket(InventoryCrafting inv, int rocketStack, int multiplierAdd) {
+		int multiplierOld = 0;
+		final ItemStack rocket = inv.getStackInSlot(rocketStack);
+		final NBTTagCompound nbtCompound = rocket.getTagCompound();
+		NBTTagCompound newTag = new NBTTagCompound();
+		if (nbtCompound != null) {
+			newTag = nbtCompound.copy();
+			if (nbtCompound.hasKey(NBT_ANTIRAIN)) {
+				multiplierOld = nbtCompound.getInteger(NBT_ANTIRAIN);
+			}
+		}
+		if ((multiplierOld + multiplierAdd) > WaterworksConfig.rockets.clearSkyMaxMultiplier) {
+			return false;
+		}
+		newTag.setInteger(NBT_ANTIRAIN, multiplierOld + multiplierAdd);
+		this.resultItem.setTagCompound(newTag);
+		return true;
 	}
 
 	@Override
--- src/main/java/org/terpo/waterworks/item/crafting/RainRocketRecipe.java
+++ src/main/java/org/terpo/waterworks/item/crafting/RainRocketRecipe.java
@@ -1,10 +1,14 @@
 package org.terpo.waterworks.item.crafting;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.terpo.waterworks.init.WaterworksConfig;
 import org.terpo.waterworks.init.WaterworksItems;
 
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.NBTTagCompound;
@@ -14,73 +18,100 @@ import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class RainRocketRecipe extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
+	private static final String NBT_RAIN = "RAIN";
 	private ItemStack resultItem = ItemStack.EMPTY;
+	private final List<Item> validItems;
+
+	public RainRocketRecipe() {
+		super();
+		this.validItems = Arrays.asList(Items.FIREWORKS, WaterworksItems.firework_rain, Items.GHAST_TEAR);
+	}
+
 	@Override
 	public boolean matches(InventoryCrafting inv, World worldIn) {
 		this.resultItem = ItemStack.EMPTY;
 
 		int rocketStack = -1;
-		boolean isFireworks = false;
-		boolean isRainRocket = false;
+		int isFireworks = 0;
+		int isRainRocket = 0;
 		int multiplierAdd = 0;
 		for (int i = 0; i < inv.getSizeInventory(); i++) {
-			final ItemStack itemstack = inv.getStackInSlot(i);
+			final ItemStack stack = inv.getStackInSlot(i);
+			final Item item = stack.getItem();
 
-			if (itemstack.getItem() == Items.FIREWORKS) {
-				isFireworks = true;
-				rocketStack = i;
-				continue;
+			if (validateStack(stack, item)) {
+				return false;
 			}
-			if (itemstack.getItem() == WaterworksItems.firework_rain) {
-				isRainRocket = true;
-				rocketStack = i;
-				continue;
+
+			if (item == Items.FIREWORKS) {
+				isFireworks++;
 			}
-			if (itemstack.getItem() == Items.GHAST_TEAR) {
+
+			if (item == WaterworksItems.firework_rain) {
+				isRainRocket++;
+			}
+
+			if (item == Items.FIREWORKS || item == WaterworksItems.firework_rain) {
+				rocketStack = i;
+			} else if (item == Items.GHAST_TEAR) {
 				multiplierAdd++;
 			}
 		}
 
-		if ((isRainRocket && isFireworks) || multiplierAdd <= 0) {
+		if ((isRainRocket + isFireworks) > 1 || multiplierAdd == 0) {
 			return false;
 		}
+
 		this.resultItem = new ItemStack(WaterworksItems.firework_rain);
 
-		int multiplierOld = 0;
-		if (isFireworks) {
-			final ItemStack rocket = inv.getStackInSlot(rocketStack);
-			NBTTagCompound nbtCompound = rocket.getTagCompound();
-
-			if (nbtCompound == null) {
-				nbtCompound = new NBTTagCompound();
-			}
-			if (multiplierOld + multiplierAdd > WaterworksConfig.rockets.rainMaxMultiplier) {
-				return false;
-			}
-			nbtCompound.setInteger("RAIN", multiplierOld + multiplierAdd);
-			this.resultItem.setTagCompound(nbtCompound);
-			return true;
+		if (isFireworks > 0) {
+			return handleFireworksRocket(inv, rocketStack, multiplierAdd);
 		}
 
-		if (isRainRocket) {
-			final ItemStack rocket = inv.getStackInSlot(rocketStack);
-			final NBTTagCompound nbtCompound = rocket.getTagCompound();
-			NBTTagCompound newTag = new NBTTagCompound();
-			if (nbtCompound != null) {
-				newTag = nbtCompound.copy();
-				if (nbtCompound.hasKey("RAIN")) {
-					multiplierOld = nbtCompound.getInteger("RAIN");
-				}
-			}
-			if ((multiplierOld + multiplierAdd) > WaterworksConfig.rockets.rainMaxMultiplier) {
-				return false;
-			}
-			newTag.setInteger("RAIN", multiplierOld + multiplierAdd);
-			this.resultItem.setTagCompound(newTag);
-			return true;
+		if (isRainRocket > 0) {
+			return handleRainRocket(inv, rocketStack, multiplierAdd);
 		}
 
 		return false;
+	}
+
+	protected boolean validateStack(ItemStack stack, final Item item) {
+		return !stack.isEmpty() && !this.validItems.contains(item);
+	}
+
+	protected boolean handleRainRocket(InventoryCrafting inv, int rocketStack, int multiplierAdd) {
+		int multiplierOld = 0;
+		final ItemStack rocket = inv.getStackInSlot(rocketStack);
+		final NBTTagCompound nbtCompound = rocket.getTagCompound();
+		NBTTagCompound newTag = new NBTTagCompound();
+		if (nbtCompound != null) {
+			newTag = nbtCompound.copy();
+			if (nbtCompound.hasKey(NBT_RAIN)) {
+				multiplierOld = nbtCompound.getInteger(NBT_RAIN);
+			}
+		}
+		if ((multiplierOld + multiplierAdd) > WaterworksConfig.rockets.rainMaxMultiplier) {
+			return false;
+		}
+		newTag.setInteger(NBT_RAIN, multiplierOld + multiplierAdd);
+		this.resultItem.setTagCompound(newTag);
+		return true;
+	}
+
+	protected boolean handleFireworksRocket(InventoryCrafting inv, int rocketStack, int multiplierAdd) {
+		final int multiplierOld = 0;
+		final ItemStack rocket = inv.getStackInSlot(rocketStack);
+		NBTTagCompound nbtCompound = rocket.getTagCompound();
+
+		if (nbtCompound == null) {
+			nbtCompound = new NBTTagCompound();
+		}
+		if (multiplierOld + multiplierAdd > WaterworksConfig.rockets.rainMaxMultiplier) {
+			return false;
+		}
+		nbtCompound.setInteger(NBT_RAIN, multiplierOld + multiplierAdd);
+		this.resultItem.setTagCompound(nbtCompound);
+		return true;
 	}
 
 	@Override
