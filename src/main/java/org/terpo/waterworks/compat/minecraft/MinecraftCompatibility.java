b7c4201e241af1720e3a20246d1b7537bffe0854
Terpo <mail@terpo.org>
2020-01-14 19:24:05 +0100

    fix NPE when registering behavior without rocket #76

--- src/main/java/org/terpo/waterworks/compat/minecraft/MinecraftCompatibility.java
+++ src/main/java/org/terpo/waterworks/compat/minecraft/MinecraftCompatibility.java
@@ -2,6 +2,7 @@ package org.terpo.waterworks.compat.minecraft;
 
 import org.terpo.waterworks.entity.item.EntityFireworkRocketAntiRain;
 import org.terpo.waterworks.entity.item.EntityFireworkRocketRain;
+import org.terpo.waterworks.init.WaterworksConfig;
 import org.terpo.waterworks.init.WaterworksItems;
 
 import net.minecraft.block.BlockDispenser;
@@ -12,10 +13,14 @@ import net.minecraft.world.World;
 public class MinecraftCompatibility {
 
 	public static void registerWeatherRocketDispenserBehavior() {
-		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(WaterworksItems.firework_anti_rain,
-				new FireworkRocketAntiRainDispenserBehavior(MinecraftCompatibility::spawnAntiRainRocket));
-		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(WaterworksItems.firework_rain,
-				new FireworkRocketRainDispenserBehavior(MinecraftCompatibility::spawnRainRocket));
+		if (WaterworksConfig.register.antiRainRocket) {
+			BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(WaterworksItems.firework_anti_rain,
+					new FireworkRocketAntiRainDispenserBehavior(MinecraftCompatibility::spawnAntiRainRocket));
+		}
+		if (WaterworksConfig.register.rainRocket) {
+			BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(WaterworksItems.firework_rain,
+					new FireworkRocketRainDispenserBehavior(MinecraftCompatibility::spawnRainRocket));
+		}
 	}
 
 	private static EntityFireworkRocketRain spawnRainRocket(World worldIn, Vec3d v, ItemStack itemstack) {
