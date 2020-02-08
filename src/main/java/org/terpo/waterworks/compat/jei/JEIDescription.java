6aefe9b7450e9c8e248a971cf909973ae46c990b
Terpo <mail@terpo.org>
2020-02-08 13:26:49 +0100

    fix JEI descriptions missing

--- src/main/java/org/terpo/waterworks/compat/jei/JEIDescription.java
+++ src/main/java/org/terpo/waterworks/compat/jei/JEIDescription.java
@@ -11,6 +11,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraftforge.registries.ForgeRegistryEntry;
 
 public class JEIDescription {
 	static IRecipeRegistration registry;
@@ -18,26 +19,26 @@ public class JEIDescription {
 	public static void addJEIDescriptions(IRecipeRegistration modRegistry) {
 		JEIDescription.registry = modRegistry;
 
-		final HashMap<Object, String> descriptionMap = new HashMap<>();
+		final HashMap<ForgeRegistryEntry<?>, String> descriptionMap = new HashMap<>();
 		if (Config.pump.getGroundwaterPumpDescription()) {
-			descriptionMap.put(Registration.groundwaterPumpBlock, "tile.groundwater_pump.description");
-			descriptionMap.put(Registration.waterPipeBlock, "tile.water_pipe.description");
+			descriptionMap.put(Registration.groundwaterPumpBlock.get(), "tile.groundwater_pump.description");
+			descriptionMap.put(Registration.waterPipeBlock.get(), "tile.water_pipe.description");
 		}
 		if (Config.rainCollection.getRainCollectorDescription()) {
-			descriptionMap.put(Registration.rainCollectorBlock, "tile.rain_collector.description");
-			descriptionMap.put(Registration.rainCollectorControllerBlock, "tile.rain_collector_controller.description");
+			descriptionMap.put(Registration.rainCollectorBlock.get(), "tile.rain_collector.description");
+			descriptionMap.put(Registration.rainCollectorControllerBlock.get(), "tile.rain_collector_controller.description");
 		}
 		if (Config.rainCollection.getWoodenRainTankDescription()) {
-			descriptionMap.put(Registration.rainTankWoodBlock, "tile.rain_tank_wood.description");
+			descriptionMap.put(Registration.rainTankWoodBlock.get(), "tile.rain_tank_wood.description");
 		}
 
 		if (Config.rockets.getFireworkRocketsDescription()) {
-			descriptionMap.put(Registration.fireworkRainItem, "item.firework_rain.description");
-			descriptionMap.put(Registration.fireworkAntiRainItem, "item.firework_anti_rain.description");
+			descriptionMap.put(Registration.fireworkRainItem.get(), "item.firework_rain.description");
+			descriptionMap.put(Registration.fireworkAntiRainItem.get(), "item.firework_anti_rain.description");
 		}
 
 		if (Config.rainCollection.getWrenchDescription()) {
-			descriptionMap.put(Registration.pipeWrenchItem, "item.pipe_wrench.description");
+			descriptionMap.put(Registration.pipeWrenchItem.get(), "item.pipe_wrench.description");
 		}
 		if (Config.rockets.getFireworkChargeDescription()) {
 			descriptionMap.put(Items.FIREWORK_STAR, "item.firework_charge.description");
